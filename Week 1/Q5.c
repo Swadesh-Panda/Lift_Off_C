5cdab171ff24cf8b38c4e13d2d4fb60fdeeecb09
Swadesh-Panda <78480951+Swadesh-Panda@users.noreply.github.com>
2021-02-04 21:59:40 +0530

    Update Q5.c

--- Week 1/Q5.c
+++ Week 1/Q5.c
@@ -20,7 +20,22 @@ void main()
         break;
     case 'u' :
         printf("Vowel");
-        break;    
+        break;
+    case 'A' :
+        printf("Vowel");
+        break;
+    case 'E' :
+        printf("Vowel");
+        break;
+    case 'I' :
+        printf("Vowel");
+        break;
+    case 'O' :
+        printf("Vowel");
+        break;
+    case 'U' :
+        printf("Vowel");
+        break;
     default:
         printf("Consonant");
         break;
